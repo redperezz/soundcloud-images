a98ebd4c870dd2b5fd649f8309b65b9e56653386
redperezz <kaicruzz91@gmail.com>
2021-01-12 17:41:11 -0800

    added dotenv config

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../../database/pg.js');
 const redis = require('redis');
+require('dotenv').config();
 
 const client = redis.createClient(process.env.REDIS_PORT);
 
